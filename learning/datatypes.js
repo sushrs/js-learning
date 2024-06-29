a0fef627f30375dd316cae039c4ada73786606c6
Sushila <143934893+sushrs@users.noreply.github.com>
2024-06-29 03:13:56 +0000

    datatypes

---
+++ learning/datatypes.js
@@ -0,0 +1,18 @@
+let rollNumber = 34;
+let studentName = "KK"
+const schoolName = "Govt School";
+let city;
+let marks = null;
+
+// Datatypes Coversion
+
+// console.log(rollNumber);
+
+// let stringRollNumber = String(rollNumber);//number to string conversion
+// console.log(typeof stringRollNumber);
+// console.log(stringRollNumber);
+let numberSchoolName = Number(schoolName);
+//console.log(typeof numberSchoolName);
+console.log(numberSchoolName);
+console.log(typeof city);
+console.log(typeof marks); //types of null is Object
