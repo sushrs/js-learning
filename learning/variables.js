4e057a68f29e5923fb90bbb8d9a00d07a9704ba4
Sushila <143934893+sushrs@users.noreply.github.com>
2024-06-29 03:04:24 +0000

    variables

---
+++ learning/variables.js
@@ -0,0 +1,20 @@
+const rollNumber = 5;
+let name = "sush";
+let schoolName= "ABC";
+var subject= "eng";
+
+// console.log(rollNumber);
+// console.log(name);
+// console.log(schoolName);
+//console.log(subject);
+//var isn't suitable due to block scope
+// prefer onlt let and const
+var subject= "math"; 
+// console.log(subject);
+
+//  rollNumber = 30; // TypeError: Assignment to constant variable.
+//  console.log(rollNumber);
+
+ schoolName= "XYZ";
+ console.log(schoolName);
+
